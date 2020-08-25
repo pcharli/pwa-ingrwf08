fe8005c3c345770cdfed3c44cc19dfe4eeed2da7
Pierre Charlier <lretlo@gmail.com>
2020-08-25 13:54:31 +0200

    add workbox

--- service.js
+++ service.js
@@ -10,10 +10,40 @@ self.addEventListener('activate', event => {
     return self.clients.claim();
 })
 
-self.addEventListener("fetch", event=> {
+// Mise en cache
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+if(workbox) {
+    console.log('yay Workbox is loaded')
+
+    workbox.precaching.precacheAndRoute([
+        {
+            url: "index.html"
+        },
+        {
+            url: "style.css"
+        },
+        {
+            url: "images/logo.png"
+        },
+        {
+            url: "main.js"
+        },
+        {
+            url: "https://cdnjs.cloudflare.com/ajax/libs/bulma/0.9.0/css/bulma.min.css"
+        }
+    ])
+}
+else {
+    console.log("alert", "error Workbox")
+}
+
+
+/*self.addEventListener("fetch", event=> {
    
     const requestUrl = new URL(
         event.request.url
       )
       console.log(requestUrl)
-})
+})*/

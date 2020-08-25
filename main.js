1eb29b05cf1b9907a61d349952b6b8332288d3d4
Pierre Charlier <lretlo@gmail.com>
2020-08-25 13:35:54 +0200

    add btn install

--- main.js
+++ main.js
@@ -12,3 +12,28 @@ if('serviceWorker' in navigator) {
 else {
     console.log('alert', 'Votre navigateur n\'est pas compatible avec nos services')
 }
+
+const installBtn = document.querySelector('.installer')
+
+let defferedPrompt
+
+window.addEventListener('beforeinstallprompt', e => {
+    e.preventDefault()
+    defferedPrompt = e
+    installBtn.style.display = "block"
+})
+
+installBtn.addEventListener("click", e => {
+    installBtn.style.display = 'none'
+    defferedPrompt.prompt()
+
+    defferedPrompt.userChoice.then(choiceResult => {
+        if(choiceResult.outcome === 'accepted') {
+            console.log('installation réussie')
+        }
+        else {
+            console.log('installation refusée')
+        }
+        defferedPrompt = null
+    })
+})
--- service.js
+++ service.js
@@ -1,4 +1,4 @@
-const version = '0.0.1'
+const version = '0.0.2'
 
 self.addEventListener('install', event => {
     console.log('Install' + version)
